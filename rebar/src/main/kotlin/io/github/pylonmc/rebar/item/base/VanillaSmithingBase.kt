4a7bf8ef0565682be0fe6f21dee835ed9d03ac2c
JustAHuman-xD <imjustahuman.xd@gmail.com>
2026-05-26 14:50:48 -0500

    fix smithing rebar items

---
+++ rebar/src/main/kotlin/io/github/pylonmc/rebar/item/base/VanillaSmithingBase.kt
@@ -0,0 +1,7 @@
+package io.github.pylonmc.rebar.item.base
+
+/**
+ * Allows an item to act as a normal vanilla smithing base in the vanilla smithing table.
+ * Ex: A diamond sword is the base for crafting a netherite sword
+ */
+interface VanillaSmithingBase
--- rebar/src/main/kotlin/io/github/pylonmc/rebar/item/base/VanillaSmithingMaterial.kt
+++ rebar/src/main/kotlin/io/github/pylonmc/rebar/item/base/VanillaSmithingMaterial.kt
@@ -1,6 +1,7 @@
 package io.github.pylonmc.rebar.item.base
 
 /**
- * Allows an item to act as a normal vanilla trim material in the vanilla smithing table.
+ * Allows an item to act as a normal vanilla smithing material in the vanilla smithing table.
+ * Ex: A diamond is a material used to trim armor, a netherite ingot to upgrade diamond equipment
  */
 interface VanillaSmithingMaterial
