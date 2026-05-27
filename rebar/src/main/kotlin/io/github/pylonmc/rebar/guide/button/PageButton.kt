532bde299a62bd3139ac8dbe9bb34efc2b4f69d1
JustAHuman-xD <imjustahuman.xd@gmail.com>
2026-05-27 13:28:54 -0500

    hide most components from page buttons

--- rebar/src/main/kotlin/io/github/pylonmc/rebar/guide/button/PageButton.kt
+++ rebar/src/main/kotlin/io/github/pylonmc/rebar/guide/button/PageButton.kt
@@ -3,6 +3,8 @@ package io.github.pylonmc.rebar.guide.button
 import io.github.pylonmc.rebar.guide.pages.base.GuidePage
 import io.github.pylonmc.rebar.item.builder.ItemStackBuilder
 import io.github.pylonmc.rebar.util.rebarKey
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.TooltipDisplay
 import net.kyori.adventure.text.Component
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -28,10 +30,12 @@ open class PageButton(val stack: ItemStack, val page: GuidePage) : GuideButton()
 
     constructor(material: Material, page: GuidePage) : this(ItemStack(material), page)
 
+    @Suppress("UnstableApiUsage")
     override fun getItemProvider(viewer: Player): ItemProvider = ItemStackBuilder.gui(stack.clone(), "${rebarKey("guide_page")}:${page.key}")
         .name(Component.translatable("${page.key.namespace}.guide.page.${page.key.key}"))
         .clearLore()
         .lore(Component.translatable("${page.key.namespace}.guide.button.${page.key.key}.lore", ""))
+        .set(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplay.tooltipDisplay().hiddenComponents(hiddenComponents))
 
     override fun handleClick(clickType: ClickType, player: Player, click: Click) {
         page.open(player)
@@ -40,5 +44,25 @@ open class PageButton(val stack: ItemStack, val page: GuidePage) : GuideButton()
     override fun shouldDisplay(player: Player) = page.shouldDisplay(player)
 
     override fun priority(): Double = 0.0
+
+    companion object {
+        @Suppress("UnstableApiUsage")
+        val hiddenComponents = setOf(
+            DataComponentTypes.DAMAGE, DataComponentTypes.MAX_DAMAGE, DataComponentTypes.UNBREAKABLE,
+            DataComponentTypes.ENCHANTMENTS, DataComponentTypes.STORED_ENCHANTMENTS,
+            DataComponentTypes.ATTRIBUTE_MODIFIERS,
+            DataComponentTypes.BANNER_PATTERNS, DataComponentTypes.PROVIDES_BANNER_PATTERNS,
+            DataComponentTypes.CAN_PLACE_ON, DataComponentTypes.CAN_BREAK,
+            DataComponentTypes.FOOD, DataComponentTypes.CONSUMABLE,
+            DataComponentTypes.TOOL, DataComponentTypes.WEAPON,
+            DataComponentTypes.DYED_COLOR,
+            DataComponentTypes.MAP_ID, DataComponentTypes.MAP_DECORATIONS,
+            DataComponentTypes.CHARGED_PROJECTILES, DataComponentTypes.BUNDLE_CONTENTS, DataComponentTypes.POTION_CONTENTS, DataComponentTypes.POT_DECORATIONS,
+            DataComponentTypes.CONTAINER, DataComponentTypes.CONTAINER_LOOT,
+            DataComponentTypes.TRIM,
+            DataComponentTypes.FIREWORK_EXPLOSION, DataComponentTypes.FIREWORKS,
+            DataComponentTypes.PROFILE
+        )
+    }
 
 }
